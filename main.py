aef67270e23a5ad9fd9cd57238ae6bcb1062d3a6
manne2012 <>
2021-05-13 13:35:45 +0000

    added comments

--- main.py
+++ main.py
@@ -1,54 +1,100 @@
-
-# s = "This is a single line string"
+'''
+Text Type (String)
+'''
+# s = 'This is a single line string'
 # print(s)
 # print(type(s))
 
-# s = '''this is a multiline
-# string example '''
-# print(s)
+# ===============
 
+# s = ''' this is a multiline
+# string example '''
+ # print(s)
+
+# ===============
+
+# find a character by index
 # s = 'string sample'
 # print(s[5])
 
+# slicing
 # s = 'string sample'
-# print(s[0:6])
+# print(s[1:6])
 
-# s = 'string sample'
+# ===============
+#immutable
+
+# s = ' string sample'
 # s[2] = '0'
 # print(s)
 
-# x = 123453467459586
+'''
+Numeric Type (Integer, Float, Complex)
+'''
+# x = 1234534567459586
 # print(type(x))
 
-# x = 1 + 2j
+# accurate up to 16-16 decimal places
+# x = 0.1234567898765432123445334
 # print(type(x))
 
+# x = 1 +2j
+# print(type(x))
+
+'''
+Sequence Type (List, Tuple, Range)
+'''
+# homogeneous
+# x = []
+# List
 # x = [10, 2.5, 'hello']
 # print(x[2])
 # print(x [1:3])
 
+#mutable
 # x[2] = 'Python'
 # print(x)
 
+#Tuple
+#heterogeneous
 # tuple = ()
+
+# =================
+
+#both of examples below are type tuples, without comma makes it a string.
+# tuple = ("hello")
+# tuple = "hello"
+# print(type(tuple))
+
+# =========================
+
 # tuple = ("cat", [4,3,2], (1,2,3))
 # print(tuple[2])
-# tuple = "hello",
 
-#  tuple = "hello",
-#  print(type(tuple))
+# =================
 
-# tuple = 9("cat", [4,3,2], (1,2,3))
+# immutable
+
+# tuple = ("cat", [4,3,2], (1,2,3))
 # tuple[2] = 10
 # print(tuple)
+
+# ===================
+
+# Range
 
 # x = range(20)
 # for n in x:
 #   print(n)
 
+'''
+Mapping Type (Dictionary)
+'''
 # dict = {}
 # print(type(dict)) 
 
+# ===================
+#dict is mutable
 # dict = {'name': 'Kingsley', 'age': 37}
 # print(dict['age'])
 # print(dict.get('name'))
@@ -56,18 +102,60 @@
 # dict['age'] = 26
 # print(dict)
 
+'''
+Set Types
+'''
+
+# empty set having set = {} is an empty dict
+
 # set = set()
 # print(type(set))
 
+# =================
+
+#mixed data types ( all immutable )
+
 # set = {3.2, "hi", (1,2,3)}
 # print(set)
+#TypeError: 'set' object is not subscriptable
+# print(set[0])
 
+# =================
+#no duplicates
+
+# set = {3.2, "hi", (1,2,3), "hi"}
+# print(set)
+
+# ==================
+#cannot have mutable (list)in a set
+# set = {3.2, "hi", (1,2,3), [1,2,3]}
+# unhashable type: 'list'
+# print(set)
+
+'''
+Boolean Type (True or False)
+'''
+
+# print(type(True))
+
+#boolean as numbers
+# print(True == 1)
+# print(False == 0)
+
+# interesting logic
+# print(True + True)
+
+# not boolean operator 
+# print(not True)
+# print(not False)
+
+# and boolean operator
 # print(True and False)
 # print(True and True)
 # print(False and False)
 
-
-print(True or False)
-print(True or True)
-print(False or False)
+#or boolean operator
+# print(True or False)
+# print(True or True)
+# print(False or False)
 
