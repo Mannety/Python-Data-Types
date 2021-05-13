2872c24774a3ce2983814269a06138a0da90c709
Mannety_ Dev <>
2021-05-13 11:49:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+
+# s = "This is a single line string"
+# print(s)
+# print(type(s))
+
+# s = '''this is a multiline
+# string example '''
+# print(s)
+
+# s = 'string sample'
+# print(s[5])
+
+# s = 'string sample'
+# print(s[0:6])
+
+# s = 'string sample'
+# s[2] = '0'
+# print(s)
+
+# x = 123453467459586
+# print(type(x))
+
+# x = 1 + 2j
+# print(type(x))
+
+# x = [10, 2.5, 'hello']
+# print(x[2])
+# print(x [1:3])
+
+# x[2] = 'Python'
+# print(x)
+
+# tuple = ()
+# tuple = ("cat", [4,3,2], (1,2,3))
+# print(tuple[2])
+# tuple = "hello",
+
+#  tuple = "hello",
+#  print(type(tuple))
+
+# tuple = 9("cat", [4,3,2], (1,2,3))
+# tuple[2] = 10
+# print(tuple)
+
+# x = range(20)
+# for n in x:
+#   print(n)
+
+# dict = {}
+# print(type(dict)) 
+
+# dict = {'name': 'Kingsley', 'age': 37}
+# print(dict['age'])
+# print(dict.get('name'))
+
+# dict['age'] = 26
+# print(dict)
+
+# set = set()
+# print(type(set))
+
+# set = {3.2, "hi", (1,2,3)}
+# print(set)
+
+# print(True and False)
+# print(True and True)
+# print(False and False)
+
+
+print(True or False)
+print(True or True)
+print(False or False)
+
